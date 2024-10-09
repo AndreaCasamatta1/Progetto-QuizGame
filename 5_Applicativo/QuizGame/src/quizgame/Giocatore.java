6a68f5b422008bdf6420edf7bb878eaa17569bbd
Andrea Casamatta <andrea.casamatta@CPT.local>
2024-10-09 15:35:40 +0200

    commit implementazione

---
+++ 5_Applicativo/QuizGame/src/quizgame/Giocatore.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quizgame;
+
+/**
+ *
+ * @author Andrea.casamatta
+ */
+public class Giocatore {
+    private String nomeUtente;
+    private int punteggio;
+    public void aggiornaPunteggio(int punti){
+        
+    }
+}
