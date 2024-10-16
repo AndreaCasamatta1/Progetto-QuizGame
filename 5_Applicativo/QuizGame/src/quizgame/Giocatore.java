9cfe174c0c9ae433c3f351067a06133a12d164f3
Andrea Casamatta <andrea.casamatta@CPT.local>
2024-10-16 15:42:30 +0200

    implementazione2

--- 5_Applicativo/QuizGame/src/quizgame/Giocatore.java
+++ 5_Applicativo/QuizGame/src/quizgame/Giocatore.java
@@ -11,6 +11,28 @@ package quizgame;
 public class Giocatore {
     private String nomeUtente;
     private int punteggio;
+
+    public Giocatore(String nomeUtente, int punteggio) {
+        this.nomeUtente = nomeUtente;
+        this.punteggio = punteggio;
+    }
+    
+    public String getNomeUtente() {
+        return nomeUtente;
+    }
+
+    public void setNomeUtente(String nomeUtente) {
+        this.nomeUtente = nomeUtente;
+    }
+
+    public int getPunteggio() {
+        return punteggio;
+    }
+
+    public void setPunteggio(int punteggio) {
+        this.punteggio = punteggio;
+    }
+    
     public void aggiornaPunteggio(int punti){
         
     }
